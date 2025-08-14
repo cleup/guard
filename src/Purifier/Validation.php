bed849c7ddccea467b079334598314040290e4c8
cleup <156127069+cleup@users.noreply.github.com>
2025-08-14 18:17:00 +0400

    fix(methods): error correction, applying utility methods to invalid values (#8)
    Co-authored-by: priveted <priveted@yandex.ru>

--- src/Purifier/Validation.php
+++ src/Purifier/Validation.php
@@ -59,204 +59,206 @@ class Validation extends Ruleset
     {
         $type = $rule['type'] ?? 'string';
 
-        if ($type !== 'array' && !is_null($value)) {
-            if (is_array($type)) {
-                $count = 0;
+        if (isset($value)) {
+            if ($type !== 'array' && !is_null($value)) {
+                if (is_array($type)) {
+                    $count = 0;
 
-                foreach ($type as $typeItem) {
-                    if (!$this->isValidType($value, $typeItem)) {
-                        $count++;
+                    foreach ($type as $typeItem) {
+                        if (!$this->isValidType($value, $typeItem)) {
+                            $count++;
+                        }
+                    }
+
+                    if ($count == count($type))
+                        $this->addError(
+                            $key,
+                            "Value must be of type: " . implode(', ', $type),
+                            "incorrect_type"
+                        );
+                } else {
+                    if (!$this->isValidType($value, $type)) {
+                        $this->addError(
+                            $key,
+                            "Value must be of type: $type",
+                            "incorrect_type"
+                        );
                     }
                 }
+            }
 
-                if ($count == count($type))
+            if (isset($rule['values']) && isset($value)) {
+                $allowedValues = is_array($rule['values']) ? $rule['values'] : [$rule['values']];
+
+                if (!in_array($value, $allowedValues, true)) {
                     $this->addError(
                         $key,
-                        "Value must be of type: " . implode(', ', $type),
-                        "incorrect_type"
+                        "Value is not allowed",
+                        "value_is_not_allowed"
                     );
-            } else {
-                if (!$this->isValidType($value, $type)) {
+                }
+            }
+
+            $minMaxTypes = [
+                'number',
+                'numeric',
+                'string',
+                'int',
+                'integer',
+                'float',
+                'floating'
+            ];
+
+            $isNumber =   (
+                $type == 'number' ||
+                $type == 'numeric' ||
+                $type == 'int' ||
+                $type == 'integer' ||
+                $type == 'float' ||
+                $type == 'floating') &&
+                (
+                    is_int($value) ||
+                    is_float($value) ||
+                    is_numeric($value)
+                );
+
+            // Min
+            if (
+                isset($rule['min']) &&
+                !is_array($rule['min']) &&
+                in_array($type, $minMaxTypes)
+            ) {
+                $minError = false;
+
+                if ($isNumber)
+                    $minError = $value < $rule['min'];
+
+                if ($type == 'string')
+                    $minError = mb_strlen($value, 'UTF-8') < $rule['min'];
+
+                if ($minError) {
+                    $this->addError(
+                        $key,
+                        "The value is too small",
+                        "small_value"
+                    );
+                }
+            }
+
+            // Max
+            if (
+                isset($rule['max']) &&
+                !is_array($rule['max']) &&
+                in_array($type, $minMaxTypes)
+            ) {
+                $maxError = false;
+
+                if ($isNumber)
+                    $maxError = $value > $rule['max'];
+
+                if ($type == 'string')
+                    $maxError = mb_strlen($value, 'UTF-8') > $rule['max'];
+
+                if ($maxError) {
+                    $this->addError(
+                        $key,
+                        "The value is too high.",
+                        "high_value"
+                    );
+                }
+            }
+
+            $verifyUtils = [
+                'email'            => 'The email address has an invalid format',
+                'url'              => 'The URL has an invalid format',
+                'allowedProtocol'  => 'The protocol is not allowed',
+                'allowedHost'      => 'The host is not allowed',
+                'domain'           => 'The domain has an invalid format',
+                'ip'               => 'The IP address has an invalid format',
+                'phone'            => 'The phone number has an invalid format',
+                'dateFormat'       => 'The date has an invalid format',
+                'notEmpty'         => 'The value is empty',
+                'hexColor'         => 'The hex color is in the wrong format',
+                'rgbaColor'        => 'The rgba color is in the wrong format',
+                'rgbColor'         => 'The rgb color is in the wrong format',
+                'hslColor'         => 'The hsl color is in the wrong format',
+                'hslaColor'        => 'The hsla color is in the wrong format',
+                'cssColor'         => 'The css color is in the wrong format',
+                'latin'            => 'This meaning does not correspond to Latin',
+                'positiveNumber'   => 'The value is not a positive number',
+                'negativeNumber'   => 'The value is not a negative number',
+                'even'             => 'The value is not an even number',
+                'odd'              => 'This value is not an odd number',
+                'leapYear'         => 'This year is not a leap year',
+                'futureDate'       => 'The specified date does not exceed the current one',
+                'pastDate'         => 'The specified date does not apply to the past',
+                'today'            => 'This date is not the current one',
+                'strongPassword'   => 'This password does not meet the security requirements',
+                'palindrome'       => 'This value is not a palindrome',
+                'romanNumeral'     => 'This value is not Roman numerals',
+                'macAddress'       => 'The MAC address has an incorrect format',
+                'json'             => 'The value does not match the JSON format',
+                'containsEmoji'    => 'The value does not contain emojis',
+                'bitcoinAddress'   => 'The Bitcoin address does not match the required format.',
+                'maxLength'        => 'The value is too high.',
+                'minLength'        => 'The value is too small'
+            ];
+
+            foreach ($verifyUtils as $utilName => $utilMessage) {
+                if (array_key_exists($utilName, $rule)) {
+                    $verifyClass = __NAMESPACE__ . "\\Utils\\Valid";
+                    $arg = is_array($rule[$utilName])
+                        ? $rule[$utilName]
+                        : (!is_bool($rule[$utilName])
+                            ? $rule[$utilName] : []
+                        );
+
+                    if (method_exists($verifyClass, $utilName)) {
+                        if (
+                            !call_user_func_array([
+                                $verifyClass,
+                                $utilName
+                            ], [$value, $arg])
+                        ) {
+                            $this->addError(
+                                $key,
+                                $utilMessage,
+                                "invalid_" . mb_strtolower(
+                                    preg_replace(
+                                        '/(?<=\w)(\p{Lu})/u',
+                                        '_$1',
+                                        $utilName
+                                    ),
+                                    'UTF-8'
+                                )
+                            );
+                        }
+                    }
+                }
+            }
+
+            if (isset($rule['validate']) && is_callable($rule['validate'])) {
+                $code = 'validation_failed';
+                $result = $rule['validate']($value, $code, $this);
+                if ($result !== true) {
+                    $this->addError(
+                        $key,
+                        $result ?? 'Validation failed',
+                        $code
+                    );
+                }
+            }
+
+            if ($type === 'array' && !is_null($value)) {
+                if (!is_array($value)) {
                     $this->addError(
                         $key,
                         "Value must be of type: $type",
                         "incorrect_type"
                     );
-                }
+                } else
+                    $this->validateArray($key, $value, $rule);
             }
-        }
-
-        if (isset($rule['values']) && isset($value)) {
-            $allowedValues = is_array($rule['values']) ? $rule['values'] : [$rule['values']];
-
-            if (!in_array($value, $allowedValues, true)) {
-                $this->addError(
-                    $key,
-                    "Value is not allowed",
-                    "value_is_not_allowed"
-                );
-            }
-        }
-
-        $minMaxTypes = [
-            'number',
-            'numeric',
-            'string',
-            'int',
-            'integer',
-            'float',
-            'floating'
-        ];
-
-        $isNumber =   (
-            $type == 'number' ||
-            $type == 'numeric' ||
-            $type == 'int' ||
-            $type == 'integer' ||
-            $type == 'float' ||
-            $type == 'floating') &&
-            (
-                is_int($value) ||
-                is_float($value) ||
-                is_numeric($value)
-            );
-
-        // Min
-        if (
-            isset($rule['min']) &&
-            !is_array($rule['min']) &&
-            in_array($type, $minMaxTypes)
-        ) {
-            $minError = false;
-
-            if ($isNumber)
-                $minError = $value < $rule['min'];
-
-            if ($type == 'string')
-                $minError = mb_strlen($value, 'UTF-8') < $rule['min'];
-
-            if ($minError) {
-                $this->addError(
-                    $key,
-                    "The value is too small",
-                    "small_value"
-                );
-            }
-        }
-
-        // Max
-        if (
-            isset($rule['max']) &&
-            !is_array($rule['max']) &&
-            in_array($type, $minMaxTypes)
-        ) {
-            $maxError = false;
-
-            if ($isNumber)
-                $maxError = $value > $rule['max'];
-
-            if ($type == 'string')
-                $maxError = mb_strlen($value, 'UTF-8') > $rule['max'];
-
-            if ($maxError) {
-                $this->addError(
-                    $key,
-                    "The value is too high.",
-                    "high_value"
-                );
-            }
-        }
-
-        $verifyUtils = [
-            'email'            => 'The email address has an invalid format',
-            'url'              => 'The URL has an invalid format',
-            'allowedProtocol'  => 'The protocol is not allowed',
-            'allowedHost'      => 'The host is not allowed',
-            'domain'           => 'The domain has an invalid format',
-            'ip'               => 'The IP address has an invalid format',
-            'phone'            => 'The phone number has an invalid format',
-            'dateFormat'       => 'The date has an invalid format',
-            'notEmpty'         => 'The value is empty',
-            'hexColor'         => 'The hex color is in the wrong format',
-            'rgbaColor'        => 'The rgba color is in the wrong format',
-            'rgbColor'         => 'The rgb color is in the wrong format',
-            'hslColor'         => 'The hsl color is in the wrong format',
-            'hslaColor'        => 'The hsla color is in the wrong format',
-            'cssColor'         => 'The css color is in the wrong format',
-            'latin'            => 'This meaning does not correspond to Latin',
-            'positiveNumber'   => 'The value is not a positive number',
-            'negativeNumber'   => 'The value is not a negative number',
-            'even'             => 'The value is not an even number',
-            'odd'              => 'This value is not an odd number',
-            'leapYear'         => 'This year is not a leap year',
-            'futureDate'       => 'The specified date does not exceed the current one',
-            'pastDate'         => 'The specified date does not apply to the past',
-            'today'            => 'This date is not the current one',
-            'strongPassword'   => 'This password does not meet the security requirements',
-            'palindrome'       => 'This value is not a palindrome',
-            'romanNumeral'     => 'This value is not Roman numerals',
-            'macAddress'       => 'The MAC address has an incorrect format',
-            'json'             => 'The value does not match the JSON format',
-            'containsEmoji'    => 'The value does not contain emojis',
-            'bitcoinAddress'   => 'The Bitcoin address does not match the required format.',
-            'maxLength'        => 'The value is too high.',
-            'minLength'        => 'The value is too small'
-        ];
-
-        foreach ($verifyUtils as $utilName => $utilMessage) {
-            if (array_key_exists($utilName, $rule)) {
-                $verifyClass = __NAMESPACE__ . "\\Utils\\Valid";
-                $arg = is_array($rule[$utilName])
-                    ? $rule[$utilName]
-                    : (!is_bool($rule[$utilName])
-                        ? $rule[$utilName] : []
-                    );
-
-                if (method_exists($verifyClass, $utilName)) {
-                    if (
-                        !call_user_func_array([
-                            $verifyClass,
-                            $utilName
-                        ], [$value, $arg])
-                    ) {
-                        $this->addError(
-                            $key,
-                            $utilMessage,
-                            "invalid_" . mb_strtolower(
-                                preg_replace(
-                                    '/(?<=\w)(\p{Lu})/u',
-                                    '_$1',
-                                    $utilName
-                                ),
-                                'UTF-8'
-                            )
-                        );
-                    }
-                }
-            }
-        }
-
-        if (isset($rule['validate']) && is_callable($rule['validate'])) {
-            $code = 'validation_failed';
-            $result = $rule['validate']($value, $code, $this);
-            if ($result !== true) {
-                $this->addError(
-                    $key,
-                    $result ?? 'Validation failed',
-                    $code
-                );
-            }
-        }
-
-        if ($type === 'array' && !is_null($value)) {
-            if (!is_array($value)) {
-                $this->addError(
-                    $key,
-                    "Value must be of type: $type",
-                    "incorrect_type"
-                );
-            } else
-                $this->validateArray($key, $value, $rule);
         }
     }
 
